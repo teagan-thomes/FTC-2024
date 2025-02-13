dea5dbacbf11a418efc4f289d6a31ebf26eb39e3
Teagan Thomes <teagan@itsys.pro>
2025-02-12 18:34:06 -0600

    tried fixing movebackward function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/HorizontalSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/HorizontalSlide.java
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode.Components;
+
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -16,7 +17,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 import java.util.Objects;
 
 public class HorizontalSlide {
-//    private DcMotorEx slideMotor;
+    // private DcMotorEx slideMotor;
     private Servo leftLinkage;
     private Servo rightLinkage;
 
@@ -26,16 +27,17 @@ public class HorizontalSlide {
     private final HardwareMap hardwareMap;
     private final Telemetry telemetry;
     double speed = 0.01;
+    private ElapsedTime timer;
+    private static final double MOVE_BACK_DELAY_MS = 500; // 500ms delay
+    private boolean isWaitingToMoveBack = false;
 
-
-//    int maxSlidePosition = 650; // 675
-//    private final Gamepad gamepad1;
-//    private final Gamepad gamepad2;
-
+    // int maxSlidePosition = 650; // 675
+    // private final Gamepad gamepad1;
+    // private final Gamepad gamepad2;
 
     public HorizontalSlide(OpMode opMode, double currentLimit) {
-//        slideMotor = opMode.hardwareMap.get(DcMotorEx.class, "slideMotor");
-//        slideMotor.setDirection(DcMotorEx.Direction.REVERSE);
+        // slideMotor = opMode.hardwareMap.get(DcMotorEx.class, "slideMotor");
+        // slideMotor.setDirection(DcMotorEx.Direction.REVERSE);
 
         leftLinkage = opMode.hardwareMap.get(Servo.class, "leftLinkage");
         rightLinkage = opMode.hardwareMap.get(Servo.class, "rightLinkage");
@@ -44,37 +46,50 @@ public class HorizontalSlide {
         this.currentLimit = currentLimit;
         this.telemetry = opMode.telemetry;
         this.hardwareMap = opMode.hardwareMap;
-
+        this.timer = new ElapsedTime();
     }
+
     public void moveForward() {
         leftLinkage.setPosition(0.34);
         rightLinkage.setPosition(0.3);
     }
+
     public void moveBackward() {
         leftLinkage.setPosition(0.9);
         rightLinkage.setPosition(0.94);
     }
+
     public void moveForwardGradually() {
-//        leftLinkage.setPosition(Math.max(0.14, leftLinkage.getPosition() - speed));
-//        rightLinkage.setPosition(Math.max(0.1, rightLinkage.getPosition() - speed));
+        // leftLinkage.setPosition(Math.max(0.14, leftLinkage.getPosition() - speed));
+        // rightLinkage.setPosition(Math.max(0.1, rightLinkage.getPosition() - speed));
         leftLinkage.setPosition(leftLinkage.getPosition() - speed);
         rightLinkage.setPosition(rightLinkage.getPosition() - speed);
     }
+
     public void moveBackwardGradually() {
-//        leftLinkage.setPosition(Math.min(0.9, leftLinkage.getPosition() + speed));
-//        rightLinkage.setPosition(Math.min(0.94, rightLinkage.getPosition() + speed));
+        // leftLinkage.setPosition(Math.min(0.9, leftLinkage.getPosition() + speed));
+        // rightLinkage.setPosition(Math.min(0.94, rightLinkage.getPosition() + speed));
         leftLinkage.setPosition(leftLinkage.getPosition() + speed);
-        rightLinkage.setPosition(rightLinkage.getPosition() + speed);;
+        rightLinkage.setPosition(rightLinkage.getPosition() + speed);
+        ;
     }
 
     public void checkInputs(boolean extend, boolean retract, double extendGradual, double retractGradual) {
         if (extend) {
             moveForward();
-        }
-        else if (retract && Objects.equals(Intake.getWristPosition(), "up")) {
-            moveBackward();
-        }
-        else {
+            isWaitingToMoveBack = false;
+        } else if (retract && Objects.equals(Intake.getWristPosition(), "up")) {
+            if (!isWaitingToMoveBack) {
+                timer.reset();
+                isWaitingToMoveBack = true;
+            }
+
+            if (timer.milliseconds() >= MOVE_BACK_DELAY_MS) {
+                moveBackward();
+                isWaitingToMoveBack = false;
+            }
+        } else {
+            isWaitingToMoveBack = false;
             // GRADUAL movement using triggers
             if (extendGradual > 0.1) {
                 moveForwardGradually();
@@ -89,81 +104,81 @@ public class HorizontalSlide {
         return leftLinkage.getPosition();
     }
 
-//    public void moveForward() {
-//        if (slideMotor.getCurrentPosition() < maxSlidePosition) {
-//            slideMotor.setPower(power);
-//        } else {
-//            stopMotor();
-//        }
-//    }
-//
-//    public void moveBackward() {
-//        if (slideMotor.getCurrent(CurrentUnit.AMPS) < currentLimit) {
-//            slideMotor.setPower(-power);
-//        } else {
-//            stopMotor();
-//        }
-//    }
-//
-//    public void goToPosition(int position) {
-//        while (slideMotor.getCurrentPosition() < position) {
-//            slideMotor.setPower(1);
-//        }
-//        stopMotor();
-//    }
-//
-//    public void goToRest() {
-//        while (slideMotor.getCurrentPosition() > 5) {
-//            slideMotor.setPower(-1);
-//        }
-//        stopMotor();
-//    }
+    // public void moveForward() {
+    // if (slideMotor.getCurrentPosition() < maxSlidePosition) {
+    // slideMotor.setPower(power);
+    // } else {
+    // stopMotor();
+    // }
+    // }
+    //
+    // public void moveBackward() {
+    // if (slideMotor.getCurrent(CurrentUnit.AMPS) < currentLimit) {
+    // slideMotor.setPower(-power);
+    // } else {
+    // stopMotor();
+    // }
+    // }
+    //
+    // public void goToPosition(int position) {
+    // while (slideMotor.getCurrentPosition() < position) {
+    // slideMotor.setPower(1);
+    // }
+    // stopMotor();
+    // }
+    //
+    // public void goToRest() {
+    // while (slideMotor.getCurrentPosition() > 5) {
+    // slideMotor.setPower(-1);
+    // }
+    // stopMotor();
+    // }
 
-//    public void checkInputs(
-//            Boolean extend,
-//            Boolean retract,
-//            Boolean reset
-//    ) {
-//        if (extend) {
-//            moveForward();
-//        } else if (retract) {
-//            moveBackward();
-//        } else {
-//            stopMotor();
-//        }
-//        telemetry.addData("hSlide Power: ", slideMotor.getCurrent(CurrentUnit.AMPS));
-//        telemetry.addData("hSlide Position: ", slideMotor.getCurrentPosition());
-//
-//        if(reset) {
-//            resetEncoder();
-//        }
-//
-//    }
+    // public void checkInputs(
+    // Boolean extend,
+    // Boolean retract,
+    // Boolean reset
+    // ) {
+    // if (extend) {
+    // moveForward();
+    // } else if (retract) {
+    // moveBackward();
+    // } else {
+    // stopMotor();
+    // }
+    // telemetry.addData("hSlide Power: ", slideMotor.getCurrent(CurrentUnit.AMPS));
+    // telemetry.addData("hSlide Position: ", slideMotor.getCurrentPosition());
+    //
+    // if(reset) {
+    // resetEncoder();
+    // }
+    //
+    // }
 
-//    public void resetEncoder() {
-////        while(slideMotor.getCurrent(CurrentUnit.AMPS) < currentLimit) {
-////            moveBackward();
-////            telemetry.addData("moving back", "to reset encoder");
-////            telemetry.update();
-////        }
-////        telemetry.addData("hit limit", getPos());
-////        telemetry.update();
-//
-//        slideMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-//        slideMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-//    }
-//
-//    // Stops the slide
-//    public void stopMotor() {
-//        slideMotor.setPower(0);
-//    }
-//
-//
-//    public int getPos() {
-//        return slideMotor.getCurrentPosition();
-//    }
-//
-//    public void setPower(double power) {
-//        slideMotor.setPower(power);
-//    }
+    // public void resetEncoder() {
+    //// while(slideMotor.getCurrent(CurrentUnit.AMPS) < currentLimit) {
+    //// moveBackward();
+    //// telemetry.addData("moving back", "to reset encoder");
+    //// telemetry.update();
+    //// }
+    //// telemetry.addData("hit limit", getPos());
+    //// telemetry.update();
+    //
+    // slideMotor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+    // slideMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+    // }
+    //
+    // // Stops the slide
+    // public void stopMotor() {
+    // slideMotor.setPower(0);
+    // }
+    //
+    //
+    // public int getPos() {
+    // return slideMotor.getCurrentPosition();
+    // }
+    //
+    // public void setPower(double power) {
+    // slideMotor.setPower(power);
+    // }
 }
